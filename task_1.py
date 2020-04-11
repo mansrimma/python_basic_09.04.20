0334241bce2fced149ae76b6d7e99ff132c6449f
Rimka <mansrimma@yandex.ru>
2020-04-11 21:33:54 +0500

    home work №1 task_1.py

---
+++ task_1.py
@@ -0,0 +1,10 @@
+numb_1 = 25
+numb_2 = 16.25
+color = 'yellow'
+print(numb_1, numb_2, color)
+
+last_name = input('Введите фамилию: ')
+name = input('Введите имя: ')
+number_1 = int(input('Введите целое число: '))
+number_2 = float(input('Введите вещественное число: '))
+print('Сумма равна: ', number_1 + number_2)
